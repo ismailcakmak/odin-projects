650dd29cce9eccfe1b8aaa524ea7e046ad6cbd2c
ismailcakmak <smalcakmak@gmail.com>
2025-01-31 22:15:54 +0300

    added basic hovering

--- odin-sketch-pad/script.js
+++ odin-sketch-pad/script.js
@@ -1,18 +1,34 @@
 
-createGrid(container, tileNumbySide){
-    
-    for(let i=0; i<tileNumbySide; i++) {
+function createGrid(container, tileNumbySide) {
+    for(let i=0; i<Math.pow(tileNumbySide,2); i++) {
         const tile = document.createElement("div");
         tile.classList.add("tile");
         container.appendChild(tile);
     }
 }
 
+function applyHoveringEffect(hoveringEffect){
+
+    const tileList = document.querySelectorAll(".container div");
+
+    tileList.forEach((tile)=>{
+        tile.addEventListener("mouseover", () => {hoveringEffect(tile);
+        });
+    });
+}
+
+function hoveringEffect(tile){
+    tile.classList.add("hovered-tile");
+}
 
 function main() {
-    const container = document.querySelector(".container");
+
     const tileNumbySide = 16; //this means there will be 16x16 of grid
-    createGrid(container, tileNumbySide); //this always add to .container div
+    
+    const container = document.querySelector(".container");
+    createGrid(container, tileNumbySide); //this always add to .container div 
+    applyHoveringEffect(hoveringEffect);
+
 }
 
 main();
