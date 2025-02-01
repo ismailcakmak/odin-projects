d2a3a51c7bf96bb649b410aebf21c38c5de0bba3
ismailcakmak <smalcakmak@gmail.com>
2025-02-01 09:32:34 +0300

    variables embed into code

--- odin-sketch-pad/script.js
+++ odin-sketch-pad/script.js
@@ -1,11 +1,17 @@
 
-function createGrid(container, tileNumbySide) {
+function createGrid(container, tileNumbySide, tileDimension) {
+
     for(let i=0; i<Math.pow(tileNumbySide,2); i++) {
         const tile = document.createElement("div");
+
         tile.classList.add("tile");
+        tile.style.height=`${tileDimension}px`;
+        tile.style.width=`${tileDimension}px`;
+
         container.appendChild(tile);
     }
 }
+
 
 function applyHoveringEffect(hoveringEffect){
 
@@ -21,12 +27,32 @@ function hoveringEffect(tile){
     tile.classList.add("hovered-tile");
 }
 
+function calculateTileDim(totalGridwidht,tileNumbySide, gapBtwTiles) {
+
+    let totalTileWidhtBySide = totalGridwidht-((tileNumbySide-1)*gapBtwTiles)
+    let onetileWidht = totalTileWidhtBySide/tileNumbySide;
+
+    return onetileWidht
+}
+
+function calculateGapBtwTiles(tileNumbySide){
+    gapBtwTiles = (tileNumbySide>50) ? 0.5 : 1;
+    return gapBtwTiles;
+}
+
 function main() {
 
-    const tileNumbySide = 16; //this means there will be 16x16 of grid
+    const totalGridwidht = 500;
+    const tileNumbySide = 30; //this means there will be 16x16 of grid
+
+    const gapBtwTiles = calculateGapBtwTiles(tileNumbySide);
+
+
+    const tileDimension = calculateTileDim(totalGridwidht, tileNumbySide, gapBtwTiles);
     
     const container = document.querySelector(".container");
-    createGrid(container, tileNumbySide); //this always add to .container div 
+    container.style.gap = `${gapBtwTiles}px`;
+    createGrid(container, tileNumbySide, tileDimension); //this always add to .container div 
     applyHoveringEffect(hoveringEffect);
 
 }
