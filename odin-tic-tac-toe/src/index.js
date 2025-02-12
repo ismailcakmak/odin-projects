b5ed81cdcdf4c33719607b746fce2e21af03fdd2
ismailcakmak <smalcakmak@gmail.com>
2025-02-12 15:24:08 +0300

    proper testing and babel and webpack setup on console tic-tac-toe

--- odin-tic-tac-toe/src/index.js
+++ odin-tic-tac-toe/src/index.js
@@ -1,9 +1,9 @@
-
-const prompt = require('prompt-sync')();
-
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+import { fileURLToPath } from 'url';
 
 //helper functions
-function getRowWinner(arr){
+function getRowStatus(arr, board){
 
     //return "none", "player1" or "player2"
 
@@ -12,100 +12,119 @@ function getRowWinner(arr){
     
     const row = arr.map((idx)=>board[idx])
 
-    if (row.some((element)=> element===" ")) return "none";
-
-    return (areArraysEqual(row, ["x","x","x"])) ?  "player1" : "player2";
+    if (areArraysEqual(row, ["X","X","X"])) return "player1";
+    else if (areArraysEqual(row, ["O","O","O"])) return "player2";
+    else return "none";
 }
 
 
-function isGameBoardFull(){
-    return board.every((element) => {element!==" "})
+
+function isGameBoardFull(board){
+    return board.every((element) => {element !== " "})
 }
 
 
-function gameObject(player1, player2, board) {
+function isThereAWinner(rowStatusList){
+
+    if (rowStatusList.some((rowStatus)=>rowStatus === "player1")){
+        //player1 won the game
+        return "player1";
+    }
+
+    else if(rowStatusList.some((rowStatus)=>rowStatus==="player2")) {
+        //player2 won the game
+        return "player2";
+    }
+
+    else{
+        return "none";
+    }
+}
+
+
+function GameObject(gameBoard) {
     
-    this.players = [player1,player2],
     this.isFinished = false,
     this.winner = null,
+    this.gameBoard = gameBoard, // Store the reference
 
     this.evaluate = function(){
 
         //update isFinished and winner properly
+        const rowList = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+        const rowStatusList = rowList.map((row)=>getRowStatus(row,gameBoard.board));
+        
+        console.log("rowStatusList", rowStatusList);
+        const winnerStatus = isThereAWinner(rowStatusList);
+        console.log("winnerStatus", winnerStatus);
 
-        const idxToCheck = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [6,7,8]];
-        const rowStatusList = idxToCheck.map((row)=>getRowWinner(row));
-
-        if (rowStatusList.some((rowStatus)=>{rowStatus==="player1"})){
+        if (winnerStatus==="player1"){
             //player1 won the game
-
             this.isFinished = true;
-            this.winner = this.players[0];
+            this.winner = "player1";
         }
 
-        else if(rowStatusList.some((rowStatus)=>{rowStatus==="player2"})) {
-            //player2 won the game
-
+        if (winnerStatus==="player2"){
+            //player1 won the game
             this.isFinished = true;
-            this.winner = this.players[1];
+            this.winner = "player2";
         }
 
-        else if (isGameBoardFull() && rowStatusList.every((rowStatus)=>(rowStatus==="null"))) {
-            //there is a draw
-
+        if (winnerStatus==="none" && isGameBoardFull(gameBoard.board)){
+            //player1 won the game
             this.isFinished = true;
-            this.winner = "draw";
-        }
-
-        else {
-            //no winner yet, game is still continue
+            this.winner = "none";
         }
     }
 };
 
 
-function Player(name, board, sign){
+function Player(name, symbol){
     this.name = name;
+    this.symbol = symbol;
     
-    this.play = function() {
-        let idx = Number(prompt(`${name} do your move :`));
-        board[idx] = sign; 
+    this.play = function(board) {
+        let idx = Number(prompt(`${this.name} do your move :`));
+        board[idx] = this.symbol; 
     }
 }
 
-function gameBoard2() {
+function GameBoard() {
     this.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     this.display = function() {
-        const str = `${this.board[0]} ${this.board[1]} ${this.board[2]}
-                     ${this.board[3]} ${this.board[4]} ${this.board[5]}
-                     ${this.board[6]} ${this.board[7]} ${this.board[8]}`
+        const str = `${this.board[0]}|${this.board[1]}|${this.board[2]}\n${this.board[3]}|${this.board[4]}|${this.board[5]}\n${this.board[6]}|${this.board[7]}|${this.board[8]}`
         console.log(str);
-    }
+    };
 }
+
 
 function main() {
 
     console.log("hello there");
     
-    const gameBoard = new gameBoard2();
+    const gameBoard = new GameBoard();
 
-    const player1 = new Player("ismail", gameBoard.board, "X");
-    const player2 = new Player("elif", gameBoard.board, "O");
+    const player1 = new Player("ismail", "X");
+    const player2 = new Player("elif", "O");
 
-    const game = new gameObject(player1, player2, gameBoard.board);
+    const game = new GameObject(gameBoard);
     
     while(!game.isFinished){
-        player1.play();
-        player2.play();
         gameBoard.display();
+        player1.play(gameBoard.board);
         game.evaluate();
+
+        if(!game.isFinished){
+            player2.play(gameBoard.board);
+            game.evaluate();
+        }
     }
 
-    console.log(gameObject.winner.name);
+    gameBoard.display();
+    console.log(game.winner);
 }
 
 main();
-
     /*
     0 1 2
     3 4 5
@@ -116,4 +135,4 @@ main();
     game object -> manages gameBoard and players
     */
 
-export default gameBoard2;
+export { GameBoard, Player, GameObject, getRowStatus, isGameBoardFull, isThereAWinner};
--- odin-tic-tac-toe/src/index.test.js
+++ odin-tic-tac-toe/src/index.test.js
@@ -1,15 +1,97 @@
-import gameBoard2 from ".";
+// game.test.js
+import { deserialize } from 'v8';
+import { GameBoard, Player, GameObject, getRowStatus, isGameBoardFull, isThereAWinner} from './index.js';
 
-test('display board', () => {
-
-    const gameBoardd = new gameBoard2();
-    gameBoardd.board.forEach((index)=>gameBoardd.board[index]=="X");
-    const displayedBoard = board.display();
-
-    const targetBoard =    `X X X
-                            X X X
-                            X X X`;
-                           
-    console.log(displayedBoard);
-    expect(displayedBoard).toBe(targetBoard);
+describe('Game Components', () => {
+  test('GameBoard initializes empty', () => {
+    const board = new GameBoard();
+    expect(board.board.every(cell => cell === " ")).toBe(true);
   });
+
+  test('Player can be created with correct properties', () => {
+    const board = new GameBoard();
+    const player = new Player("Test", "X");
+    expect(player.name).toBe("Test");
+    expect(player.symbol).toBe("X");
+  });
+
+  test('GameObject can run a game', () => {
+    const board = new GameBoard();
+    const player1 = new Player("P1", "X");
+    const player2 = new Player("P2", "O");
+    const game = new GameObject(player1, player2, board);
+    
+    game.doMove();
+    expect(game.isFinished).toBe(false);
+  });
+});
+
+
+test('Board display properly', ()=>{
+  const board = new GameBoard();
+  
+  board.board.forEach((_,index)=>{
+    board.board[index] = "X";
+  });
+
+  let targetBoard = `X|X|X\nX|X|X\nX|X|X`;
+
+  expect(console.log(targetBoard)).toBe(board.display());
+});
+
+
+describe("Game Logic", () => {
+  
+  describe("getting row status correctly (getRowStatus)", () => {
+    //this is suite for testing getRowStatus that has return options of "player1", "plater2", "none"
+    //therefore there are 
+    const rowList = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+    const boardList = [
+      ["X", "X", "X", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", "X", "X", "X", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", "X", "X", "X"],
+      ["X", " ", " ", " ", "X", " ", " ", " ", "X"],
+      [" ", " ", "X", " ", "X", " ", "X", " ", " "],
+      ["X", " ", " ", "X", " ", " ", "X", " ", " "],
+      [" ", "X", " ", " ", "X", " ", " ", "X", " "],
+      [" ", " ", "X", " ", " ", "X", " ", " ", "X"]
+    ];
+    
+    
+    rowList.forEach((row, index) => {
+      test(`valid row for X(player1)${index}`, () => {
+        const board = boardList[index];
+        expect(getRowStatus(row, board)).toBe("player1");
+      });
+    });
+    
+    rowList.forEach((row, index) => {
+      test(`invalid row for O(player2) ${index}`, () => {
+        const board = boardList[index];
+        expect(getRowStatus(row, board)).not.toBe("player2");
+      });
+    });
+    
+    rowList.forEach((row, index) => {
+      test(`invalid row for none(no player wins) ${index}`, () => {
+        const board = boardList[index];
+        expect(getRowStatus(row, board)).not.toBe("none");
+      });
+    })
+  });
+
+  describe("getting board full or not correctly (isGameBoardFull)", ()=>{
+    let gameBoard = new GameBoard();
+
+    test("board is not full", ()=>{
+      expect(isGameBoardFull(gameBoard.board)).toBe(false)
+    });
+  });
+
+  test("winner status", ()=>{
+    let arr = ['','','','player1']
+    expect(isThereAWinner(arr)).toBe("player1");
+  });
+  
+});
+
